01090d076fe6886797d926206750ab042bf4146b
Cheatoid <Cheatoid@users.noreply.github.com>
2026-04-11 06:17:24 +0200

    pull latest annotations

--- library/Shared/metadata_gen.lua
+++ library/Shared/metadata_gen.lua
@@ -18,11 +18,11 @@
 ---@type metadata_gen
 return {
 	package_version = "0.0.23",
-	timestamp = "2026-04-11 04:16:31",
-	commit_count = 107,
+	timestamp = "2026-04-11 04:17:25",
+	commit_count = 108,
 	tag_count = 20,
 	branch_name = "main",
-	prev_hash = "5dc02221d4637b28e751602dc285cf7b8431fd8b",
+	prev_hash = "077fc59f1cf35f223ac0c8e7dee920037486016d",
 	tag = "v0.0.24",
 	owner = "Cheatoid",
 	repo = "nanos-world-vault",
